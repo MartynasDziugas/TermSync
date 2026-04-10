63ee0d229e0d7a286c0fd87f4c4e415fac3538b1
MartynasDziugas <dziugassta@gmail.com>
2026-04-10 17:16:41 +0300

    feat: add Flask UI templates and routes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import flask
+from flask import Flask
 from src.api.routes import bp
 from src.database.connection import init_db
 from config import config
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../ui/templates", static_folder="../ui/static")
     app.config.from_object(config)
 
-    app.register_blueprint(bp, url_prefix="/api")
+    app.register_blueprint(bp, url_prefix="")
 
     with app.app_context():
         init_db()
